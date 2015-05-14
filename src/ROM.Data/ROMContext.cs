43923e9cef27b52a99d7213b11f5daafc4f42d4b
ArthurAkhmerov <akhmerovarthur@gmail.com>
2015-05-14 13:06:22 +0300

    DbContext Custom binding

--- src/ROM.Data/ROMContext.cs
+++ src/ROM.Data/ROMContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel;
+using System.Data.Entity;
 using ROM.Business.Entities;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Runtime.Serialization;
@@ -26,7 +27,17 @@ namespace ROM.Data
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            // TODO: bindings...
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Ignore<PropertyChangedEventHandler>();
+            modelBuilder.Ignore<ExtensionDataObject>();
+            modelBuilder.Ignore<IIdentifiableEntity>();
+
+            modelBuilder.Entity<Account>().HasKey<int>(e => e.AccountId).Ignore(e => e.EntityId);
+            modelBuilder.Entity<Table>().HasKey<int>(e => e.TableId).Ignore(e => e.EntityId);
+            modelBuilder.Entity<Order>().HasKey<int>(e => e.OrderId).Ignore(e => e.EntityId);
+            modelBuilder.Entity<Food>().HasKey<int>(e => e.FoodId).Ignore(e => e.EntityId);
+            modelBuilder.Entity<Ingridient>().HasKey<int>(e => e.IngridientId).Ignore(e => e.EntityId);
         }
 
     }
